1a4f776564ad7ac34763eaf0e4d2cfdd319b4db9
Ishan <agarwal.ishanedu@gmail.com>
2026-04-05 17:44:45 -0400

    This is the 'tests' folder

---
+++ tests/test_rotation_utils.py
@@ -0,0 +1,11 @@
+"""
+Program Name: Lab 11
+My name: Ishan Agarwal
+Purpose: This is to better understand how tests work, and also get in the hang of the tools used.
+Starter Code: None
+Date: 04/05/2026
+"""
+
+import pytest
+from rotation_utils import adjust_rotation
+
